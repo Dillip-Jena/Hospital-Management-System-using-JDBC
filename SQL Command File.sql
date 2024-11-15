8078c43c40d33f496280cd7f2b3e52dc041b571a
Dillip-Jena <dillip.jena2004@gmail.com>
2024-11-15 15:23:37 +0530

    Update SQL Command File.sql

--- SQL Command File.sql
+++ SQL Command File.sql
@@ -4,7 +4,7 @@ USE hospital_management;
 
 -- Table for storing doctor information
 CREATE TABLE doctors(
-	doctor_id INT PRIMARY KEY AUTO_INCREMENT,
+    doctor_id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(100) NOT NULL,
     specialization VARCHAR(100) NOT NULL,
     contact_number VARCHAR(15) NOT NULL
@@ -12,7 +12,7 @@ CREATE TABLE doctors(
 
 -- Table for storing patient information
 CREATE TABLE patients(
-	patient_id INT PRIMARY KEY AUTO_INCREMENT,
+    patient_id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(100) NOT NULL,
     age INT CHECK(age > 0),
     contact_number VARCHAR(15) NOT NULL,
@@ -21,7 +21,7 @@ CREATE TABLE patients(
 
 -- Table for storing appointment information
 CREATE TABLE appointments(
-	appointment_id INT PRIMARY KEY AUTO_INCREMENT,
+    appointment_id INT PRIMARY KEY AUTO_INCREMENT,
     doctor_id INT,
     patient_id INT,
     appointment_date DATE NOT NULL,
@@ -32,7 +32,7 @@ CREATE TABLE appointments(
 
 -- Table for storing disease information
 CREATE TABLE diseases(
-	disease_id INT PRIMARY KEY auto_increment,
+    disease_id INT PRIMARY KEY auto_increment,
     patient_id INT,
     disease_name VARCHAR(50) NOT NULL,
     current_status VARCHAR(50) NOT NULL,
